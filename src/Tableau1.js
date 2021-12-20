1eb1e0a797dddd9865b1f4184a712d3ecc2a0a5e
LCkPAn <92451567+LCkPAn@users.noreply.github.com>
2021-12-20 15:51:43 +0100

    s

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -15,6 +15,7 @@ class Tableau1 extends Phaser.Scene {
         this.load.image("flame1", "asset/tree/flame1.png")
         this.load.image("blue", "asset/tree/blue.png")
         this.load.image("bulle", "asset/tree/bubble.png")
+        this.load.image("fly", "asset/tree/lanternfly.png")
     }
 
     getFrames(prefix, length) {
@@ -25,12 +26,13 @@ class Tableau1 extends Phaser.Scene {
         return frames;
     }
 
+/** crée l'animation de la flamme avec les 2 images flame1 et 2*/
     allAnims() {
         this.fireball = this.add.sprite(960, 620, "flame1")
         this.anims.create({
             key: 'flame',
             frames: this.getFrames('flame', 2),
-            frameRate: 2,
+            frameRate: 8,
             repeat: -1,
             showOnStart: true,
 
@@ -38,21 +40,38 @@ class Tableau1 extends Phaser.Scene {
         })
         this.fireball.play("flame")
     }
-    createEnemy(){
-        this.bulle = this.physics.add.sprite(Phaser.Math.Between(0, 1728), Phaser.Math.Between(1, 900), 'bulle')
-        console.log(this.car2)
+    /** Permet au lanterne de s'éclairer et de monter vers le haut */
+    createFly() {
+        let x = Phaser.Math.Between(1, 1800)
+        let y = 1000
+
+        this.fly = this.add.sprite(x,y, 'fly')
+        this.allTweens()
+        
+
+
+        /** Généere aléatoirement des particules de fleur de cerisier sur les arbres entre X et Y position */
+    }
+    createEnemyTwo() {
+        this.bulle1 = this.physics.add.sprite(Phaser.Math.Between(1300, 1650), Phaser.Math.Between(90, 100), 'bulle')
     }
 
+    createEnemy(){
+        this.bulle2 = this.physics.add.sprite(Phaser.Math.Between(810, 990), Phaser.Math.Between(160, 180), 'bulle')
+    }
+    /** Démarre ce que j'ai besoin quand la page internet s'ouvre */
     create() {
+        this.setlight=0
+        this.lights.enable();
+        this.lights.setAmbientColor(0x808080);
+
+        this.allTweens()
         this.creerFormes()
-        this.initKeyboard();
+        this.initKeyboard()
         this.etoile = this.add.sprite(-50, 300, "etoile")
         this.flamme = this.add.sprite(-70, 300, "flamme")
-        this.bulle = this.add.sprite(-100, 100, "bulle")
-
-
     }
-
+/** crée la scène*/
     creerFormes() {
 
         this.fond = this.add.image(800, 380, "fond")
@@ -78,13 +97,23 @@ class Tableau1 extends Phaser.Scene {
         this.arb3 = this.add.image(1450, 380, "arb3");
         this.grenou = this.add.image(1450, 520, "grenou")
     }
-
+    /** ce qui permet à mes images de bouger */
+allTweens(){
+        this.tweens.add({
+            targets: [this.fly, this.spotlight],
+            y: -100,
+            duration: 1500,
+            ease: 'Linear',
+            repeat: 0,
+            delay: 0,
+        })
+    }
     tweensEtoile() {
         this.etoile.x = -50
         this.tweens.add({
             targets: this.etoile,
             x: 2000,
-            duration: 1500,
+            duration: 2500,
             ease: 'Linear',
             repeat: 0,
             delay: 0,
@@ -116,35 +145,45 @@ class Tableau1 extends Phaser.Scene {
             yoyo: true,
         })
     }
-
+    /** J'appuie sur une touche et une action ce passe*/
     initKeyboard() {
         let me = this;
         this.input.keyboard.on('keydown', function (kevent) {
             switch (kevent.keyCode) {
-                case Phaser.Input.Keyboard.KeyCodes.A:
+                case Phaser.Input.Keyboard.KeyCodes.T:
                     me.tweensEtoile()
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.Z:
+                case Phaser.Input.Keyboard.KeyCodes.Y:
                     me.tweensSautPanda()
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.E:
+                case Phaser.Input.Keyboard.KeyCodes.U:
                     me.tweensSautGrenou()
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.R:
+                case Phaser.Input.Keyboard.KeyCodes.I:
                     me.allAnims()
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.T:
+                case Phaser.Input.Keyboard.KeyCodes.A:
                     me.lanon.visible=true
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.Y:
+                case Phaser.Input.Keyboard.KeyCodes.Z:
                     me.lanon1.visible=true
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.U:
+                case Phaser.Input.Keyboard.KeyCodes.E:
                     me.lanon2.visible=true
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.I:
+                case Phaser.Input.Keyboard.KeyCodes.O:
                     me.createEnemy();
-                    console.log('ennemycreated')
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.R:
+                    me.lanon.visible=true
+                    me.lanon1.visible=true
+                    me.lanon2.visible=true
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.P:
+                    me.createEnemyTwo();
+                    break;
+                case Phaser.Input.Keyboard.KeyCodes.Q:
+                    me.createFly();
                     break;
 
 
@@ -153,21 +192,23 @@ class Tableau1 extends Phaser.Scene {
         })
         this.input.keyboard.on('keyup', function (kevent) {
             switch (kevent.keyCode) {
-                case Phaser.Input.Keyboard.KeyCodes.R:
-                    me.fireball.visible=false
-                    break;
-                case Phaser.Input.Keyboard.KeyCodes.T:
+                case Phaser.Input.Keyboard.KeyCodes.A:
                     me.lanon.visible=false
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.Y:
+                case Phaser.Input.Keyboard.KeyCodes.Z:
                     me.lanon1.visible=false
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.U:
+                case Phaser.Input.Keyboard.KeyCodes.E:
                     me.lanon2.visible=false
                     break;
+                case Phaser.Input.Keyboard.KeyCodes.R:
+                    me.lanon.visible=false
+                    me.lanon1.visible=false
+                    me.lanon2.visible=false
             }
     })
 }
+
 }
 
 
